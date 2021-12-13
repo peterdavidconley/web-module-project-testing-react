60e4eac1b5f918c6d06396b0f9039b490797e45f
Peter Conley <peterdavidconley@gmail.com>
2021-12-13 16:18:04 -0500

    tested alt tag displays default image with null value

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,6 +8,36 @@ test("renders without error", () => {
     render(<Episode episode={1}/>)
 });
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders the summary test passed as prop", ()=>{
 
-test("renders default image when image is not defined", ()=>{});
+    // Arrange 
+
+    render(<Episode episode={1} summary={/Such a strange thing./i}/>)
+
+    // Act 
+
+    const summary = screen.queryByText(/Such a strange thing./i);
+
+    // Assert
+
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).not.toBeFalsy();
+
+});
+
+test("renders default image when image is not defined", ()=>{
+
+    // Arrange
+    render(<Episode episode={1} image={null}/>)
+
+    // Act 
+
+    const image = screen.getByRole('img');
+
+    // Assert
+
+    expect(image).toBeInTheDocument();
+
+
+});
